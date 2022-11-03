ce0445e41b7e61ef05a874db2c9eb1bb36f992ea
Vanessa Garcia <nessa619@msn.com>
2022-11-02 19:35:57 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//console.log(avariable1);
+const thisIsANewVariable = Math.max(5, 10) * 10 - 10;
+console.log(thisIsANewVariable)
+
+let myGreeting = "Hello World!";
+console.log(myGreeting);
+const pi = 3.14;
+
+let myNumber = 99;
+console.log(myNumber)
+console.log(Math.min(myNumber, pi));
+
+
+
+
+
